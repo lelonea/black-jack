a69a6bee696c84f13aad192ae1bf722341367751
Nastya <leloneya@gmail.com>
2021-05-23 17:14:53 +0300

    Added class game amd class player with functions shoose deck, game mode(moved in) and take card

--- main.py
+++ main.py
@@ -36,54 +36,73 @@ class BigDeck(SmallDeck):
             self.values[i] = i
 
 
-def choose_deck():
-    choose = input(
-        'Choose deck to play:\n'
-        '1 - Small deck 36 cards\n'
-        '2 - Big deck 52 cards\n\n'
-        'ENTER: '
-    )
+class Game:
+    def __init__(self):
+        self.deck = self.choose_deck()
+        self.mode = self.game_mode()
 
-    if choose == '1':
-        return SmallDeck()
-    elif choose == '2':
-        return BigDeck()
-    else:
-        print('\nWrong input, try again\n')
-        return choose_deck()
+    def choose_deck(self):
+        choose = input(
+            'Choose deck to play:\n'
+            '1 - Small deck 36 cards\n'
+            '2 - Big deck 52 cards\n\n'
+            'ENTER: '
+        )
+
+        if choose == '1':
+            return SmallDeck()
+        elif choose == '2':
+            return BigDeck()
+        else:
+            print('\nWrong input, try again\n')
+            return self.choose_deck()
+
+    def game_mode(self):
+        mode = input(
+            'Choose game mode:\n'
+            '1 - Single player\n'
+            '2 - Multiplayer\n'
+            '3 - Single player against computer player\n\n'
+            'ENTER: '
+        )
+
+        if mode == '1':
+            players_number = 1
+            return players_number
+        elif mode == '2':
+            return self.multi()
+        elif mode == '3':
+            players_number = 0
+            return players_number
+        else:
+            return 'Wrong input'
+
+    def multi(self):
+        try:
+            players_number = int(input('Enter number of players (max - 15): \n'))
+        except ValueError:
+            print('Wrong input')
+            return self.multi()
+
+        if players_number <= 15:
+            return players_number
+        else:
+            print('Maximum number of players is 15, try again')
+            return self.multi()
 
 
-def game_mode():
-    mode = input(
-                'Choose game mode:\n'
-                '1 - Single player\n'
-                '2 - Multiplayer\n'
-                '3 - Single player against computer player\n'
-                )
+class Player:
+    players_list = []
+    game = Game()
+    deck_to_play = game.deck
+    player_number = game.mode
 
-    if mode == '1':
-        players_number = 1
-        return players_number
-    elif mode == '2':
-        return multi()
-    elif mode == '3':
-        players_number = 0
-        return players_number
-    else:
-        return 'Wrong input'
+    def __init__(self, name='Name'):
+        self.name = name
+        self.players = self.players_list.append(self.name)
+        self.cards_in_hand = [self.deck_to_play.take_card(), self.deck_to_play.take_card()]
 
-
-def multi():
-    try:
-        players_number = int(input('Enter number of players (max - 15): \n'))
-    except ValueError:
-        print('Wrong input')
-        return multi()
-
-    if players_number <= 15:
-        return players_number
-    else:
-        print('Maximum number of players is 15, try again')
-        return multi()
+    def take_card(self):
+        self.cards_in_hand.append(self.deck_to_play.take_card())
 
 
